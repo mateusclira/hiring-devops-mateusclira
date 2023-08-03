3904c27e471b9070ebf0843564c3bd8d1342875b
Mateus Lira <mateusc.lira@gmail.com>
2023-08-03 10:36:53 -0300

    feature: terraform ci/cd

--- infrastructure/sg/main.tf
+++ infrastructure/sg/main.tf
@@ -3,9 +3,9 @@ resource "aws_security_group" "alb" {
   description = "Allow TLS inbound traffic"
   vpc_id      = var.vpc_id
   ingress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"] 
   }
